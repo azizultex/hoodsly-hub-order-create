3e4ee5314888f7d86f7dda3f4613489f9f220906
Saiful Islam <saifulislam265@gmail.com>
2022-02-08 18:25:18 +0600

    added order details to the rest api

--- hoodsly--hub-order-create.php
+++ hoodsly--hub-order-create.php
@@ -23,119 +23,8 @@ final class HoodslyHub{
 
     public function __construct(){
         add_action('woocommerce_thankyou', [$this, 'send_order_data'], 10, 1);
-        //add_action('admin_init', [$this, 'test']);
     }
 
-    public function test(){
-        $order = wc_get_order(26329);
-        //write_log($order);
-        $line_items = array();
-        $data = $order->get_data();
-        // write_log(get_option('thwepo_options_name_title_map'));
-        // /write_log(get_post_meta(13922,'thwepo_advanced_settings'));
-        $order_date = $order->order_date;
-        foreach ( $order->get_items() as  $item_key => $item_values ) {
-            $item_data = $item_values->get_data();
-            $new_arr = [];
-
-            $item_meta_data = $item_values->get_meta_data();
-            $formatted_meta_data = $item_values->get_formatted_meta_data( '_', true );
-           
-            foreach($item_data['meta_data'] as $key => $value){
-                if($value->get_data()['key'] == 'pa_color'){
-                    if($value->get_data()['value'] == 'custom-color-match'){
-                        $custom_color_match = "true";
-                    }
-                }
-            }
-            write_log($custom_color_match);
-            
-            $terms = get_the_terms( $item_data['product_id'], 'product_cat' );
-            foreach ( $terms as $term ) {
-                // Categories by slug
-                $product_cat_slug= $term->slug;
-            }
-            $new_arr['product_id'] = $item_data['product_id'];
-            $new_arr['variation_id'] = $item_data['variation_id'];
-            $new_arr['quantity'] = $item_data['quantity'];
-            $line_items[] = $new_arr;
-            
-        }
-        $host = parse_url(get_site_url(), PHP_URL_HOST);
-        $domains = explode('.', $host);
-        //write_log($data);
-        //write_log($formatted_meta_data);
-        foreach( $order->get_items( 'shipping' ) as $item_id => $item ){
-            /* $order_item_name             = $item->get_name();
-            $order_item_type             = $item->get_type();
-            $shipping_method_id          = $item->get_method_id(); // The method ID
-            $shipping_method_instance_id = $item->get_instance_id(); // The instance ID
-            $shipping_method_total_tax   = $item->get_total_tax();
-            $shipping_method_taxes       = $item->get_taxes(); */
-
-            $shipping_method_total       = $item->get_total();
-            $shipping_method_id          = $item->get_method_id(); // The method ID
-            $shipping_method_title       = $item->get_method_title();
-        }
-        /* $data = $order->get_data();
-        $endpoint = 'https://hoodslyhub.com/wp-json/wc/v3/orders/'; */
-        // $details_data = [
-        //     'payment_method' => $data['payment_method'],
-        //     'payment_method_title' => $data['payment_method_title'],
-        //     'customer_note' => $data['customer_note'],
-        //     'set_paid' => true,
-        //     'meta_data' => $data['meta_data'],
-        //     'billing' => [
-        //         'first_name' => $data['billing']['first_name'],
-        //         'last_name' => $data['billing']['last_name'],
-        //         'address_1' => $data['billing']['address_1'],
-        //         'address_2' => $data['billing']['address_2'],
-        //         'city' => $data['billing']['city'],
-        //         'state' => $data['billing']['state'],
-        //         'postcode' => $data['billing']['postcode'],
-        //         'country' => $data['billing']['country'],
-        //         'email' => $data['billing']['email'],
-        //         'phone' => $data['billing']['phone']
-        //     ],
-        //     'shipping' => [
-        //         'first_name' => $data['shipping']['first_name'],
-        //         'last_name' => $data['shipping']['last_name'],
-        //         'address_1' => $data['shipping']['address_1'],
-        //         'address_2' => $data['shipping']['address_2'],
-        //         'city' => $data['shipping']['city'],
-        //         'state' => $data['shipping']['state'],
-        //         'postcode' => $data['shipping']['postcode'],
-        //         'country' => $data['shipping']['country'],
-        //     ],
-        //     'line_items' => $line_items,
-        //     'shipping_lines' => [
-        //         [
-        //             'method_id' => $shipping_method_id,
-        //             'method_title' => $shipping_method_title,
-        //             'total' => $shipping_method_total
-        //         ]
-        //     ]
-        // ];
-
-        /* $rest_api_url = "https://hoodslyhub.com/wp-json/order-data/v1/hub";
-        $host = parse_url(get_site_url(), PHP_URL_HOST);
-        $domains = explode('.', $host);
-
-        $data_string = json_encode([
-            'title'    => '#13922',
-            'order_id'    => '13922',
-            'data' => $details_data,
-            'content'  => '#13922 '.'<br>'.$data['shipping']['first_name'].' '.$data['billing']['last_name'].'<br>'.$data['billing']['email'].'<br>'.$data['billing']['phone'].'<br>'. $data['shipping']['address_1'] . $data['shipping']['address_2']. ' ,'. $data['shipping']['city'].' ,'. $data['shipping']['state'].' '.$data['shipping']['postcode'].'',
-            'status'   => 'publish',
-            'origin'   => $domains[count($domains)-2],
-            'order_date'   => $order_date,
-        ]); */
-
-    /* $response = wp_remote_post( $rest_api_url, array(
-        'body'    => $data_string
-    ) ); */
-    //write_log($response);
-    }
 	/**
      * init function for single tone approach
      *
@@ -181,8 +70,10 @@ final class HoodslyHub{
             }
             $product_name = $item_values['name'];
             $new_arr['product_id'] = $item_data['product_id'];
+            $new_arr['product_name'] = $item_data['name'];
             $new_arr['variation_id'] = $item_data['variation_id'];
             $new_arr['quantity'] = $item_data['quantity'];
+            $new_arr['order_meta'] = $formatted_meta_data;
             $line_items[] = $new_arr;
         }
         foreach( $order->get_items( 'shipping' ) as $item_id => $item ){
@@ -238,12 +129,12 @@ final class HoodslyHub{
         ];
 
         $rest_api_url = "http://localhost/hoodsly_hub/wp-json/order-data/v1/hub";
-	    if ( defined( 'WP_DEBUG' ) ) {
+	    if ( defined( 'DEV_REST_API' ) ) {
 		    $api_url = DEV_REST_API;
 	    }else{
 		    $api_url = "https://hoodslyhub.com/wp-json/order-data/v1/hub";
 	    }
-        $rest_api_url = $api_url;
+        $rest_api_url = 'http://localhost/hoodsly_hub/wp-json/order-data/v1/hub';
         $host = parse_url(get_site_url(), PHP_URL_HOST);
         //$domains = explode('.', $host);
 
@@ -257,6 +148,7 @@ final class HoodslyHub{
             'origin'   => $host,
             'order_date'   => $order_date,
             'meta_data'   => $formatted_meta_data,
+            // 'order_data'   => $line_items,
             'product_name'   => $product_name,
             'product_cat'   => $product_cat_slug,
             'product_sku'   => $item_sku,
